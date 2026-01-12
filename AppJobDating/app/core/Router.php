4a428d334a45a9321b2d5fabe689099feeabd39f
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-01-12 21:36:35 +0100

    ADJD-10 Point d'entrée Public

---
+++ AppJobDating/app/core/Router.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Core;
+
+class Router {
+    
+public function dispatch()
+    {
+        // Get the requested route.
+        $requestedRoute = trim($_SERVER['REQUEST_URI'], '/') ?? '/';
+
+        $routes = self::$routes[$_SERVER['REQUEST_METHOD']];
+
+        foreach ($routes as $route => $action)
+        {
+            // Transform route to regex pattern.
+            $routeRegex = preg_replace_callback('/{\w+(:([^}]+))?}/', function ($matches)
+            {
+                return isset($matches[1]) ? '(' . $matches[2] . ')' : '([a-zA-Z0-9_-]+)';
+            }, $route);
+
+            // Add the start and end delimiters.
+            $routeRegex = '@^' . $routeRegex . '$@';
+
+            // Check if the requested route matches the current route pattern.
+            if (preg_match($routeRegex, $requestedRoute, $matches))
+            {
+                // Get all user requested path params values after removing the first matches.
+                array_shift($matches);
+                $routeParamsValues = $matches;
+
+                // Find all route params names from route and save in $routeParamsNames
+                $routeParamsNames = [];
+                if (preg_match_all('/{(\w+)(:[^}]+)?}/', $route, $matches))
+                {
+                    $routeParamsNames = $matches[1];
+                }
+
+                // Combine between route parameter names and user provided parameter values.
+                $routeParams = array_combine($routeParamsNames, $routeParamsValues);
+
+                return  $this->resolveAction($action, $routeParams);
+            }
+        }
+        return $this->abort('404 Page not found');
+    }
+
+
+}
+
---
+++ AppJobDating/app/core/Test.php
@@ -0,0 +1,11 @@
+
+<?php
+
+class TestClass {
+
+    public function hello() {
+        return "Hello PSR-4!";
+    }
+
+
+}
